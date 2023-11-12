032403e3641160f628434a42d3f595a860ae7852
Designated-Hitter <dosadola@gmail.com>
2023-11-12 11:33:51 +0900

    [ADD]practicing hydration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+"use client"
+import { useState } from 'react';
 
 export default function Home() {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h4>안녕하세요</h4>
+      <h1>안녕하세요 {counter} </h1>
+      <button onClick ={() => setCounter((prev:any) => prev + 1)}>+</button>
     </div>
   )
 }
