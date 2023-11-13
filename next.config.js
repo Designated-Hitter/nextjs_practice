6c43036ad6caf9b416f48fca804efcecbce0b551
Designated-Hitter <dosadola@gmail.com>
2023-11-13 10:15:00 +0900

    add

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: "/contact",
+                destination: "/form",
+                
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
