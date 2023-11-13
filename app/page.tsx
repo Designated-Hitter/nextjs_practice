91b9525a76ffbcf8fb28d385914c336ff54d03e9
Designated-Hitter <dosadola@gmail.com>
2023-11-13 10:14:42 +0900

    [ADD] fetching data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+"use client"
 import Seo from '../components/Seo';
+import { useEffect, useState } from "react";
+const API_KEY = "9e5d46a1720b08db381261a21fc77e33";
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async() => {
+      const { results } = await (
+        await fetch(
+        `https://api.themoviedb.org/3/movie/latest?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title ="Home"/>
-      <h1>안녕하세요</h1>
+      { !movies && <div>로딩중...</div> }
+      {movies?.map(movie => 
+        <div key={movie.id}>
+          {movie.original_title}</div>)}
     </div>
   )
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,22 +5,43 @@ import { usePathname } from 'next/navigation'
 export default function NavBar() {
     const pathname = usePathname();
     return <nav>
+    {/* public 디렉토리에 있는 그림파일은 그냥 /만 써서 불러올 수 있다. */}
+    <img src="/vercel.svg" />
     <Link className={pathname === "/" ? "active" : ""} href="/">
     Home
     </Link>
     <Link className={pathname === "/about" ? "active" : ""} href="/about">
-    About Us
+    About
     </Link>
     <style jsx global>{`
     nav {
-        background: tomato;
-    }
-    a {
-        text-decoration: none;
-    }
-    .active {
-        color: white;
-    }
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        a {
+          font-weight: 600;
+          font-size: 18px;
+          text-decoration: none;
+          display: inline;
+          
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
     `}</style>
     </nav>
 }
